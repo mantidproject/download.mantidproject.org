c31a684a0c4d054a42bd5b530fb1eaa2862a077d
Jay Rainey <jawrainey@gmail.com>
2014-05-14 15:39:33 +0100

    Update logic, formatting and added docstrings to build-site.py

--- tools/build-site.py
+++ tools/build-site.py
@@ -5,48 +5,87 @@ import jinja2
 import os
 import re
 
-THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+ROOT_DIR = os.path.join(os.path.dirname(__file__) + "../")
+RELEASE_DIR = os.path.join(ROOT_DIR,"releases/")
+PARAVIEW_DIR = os.path.join(RELEASE_DIR, "paraview/")
 
-def releases():
-  releaseDir = THIS_DIR + "/../releases"
-  releases = [] # Stores build info for each release in the "releases" folder
+def mantid_releases():
+  """
+  Reads and stores release information for each release file in the releases folder.
 
-  releaseFiles = [fileName for fileName in os.listdir(releaseDir) if "paraview" not in fileName]
-  for fileName in releaseFiles:
+  Returns:
+    list: A list that contains dictionaries of release information for each release. The list is sorted by release date.
+  """
+  releases = []
+  release_files = [name for name in os.listdir(RELEASE_DIR) if "paraview" not in name]
+  for file_name in release_files:
     release = {}
-    release['version'], release['date'] = os.path.splitext(fileName)[0].split("-",1) # Split filename into version/date
-    # Read the buildInfo names of each file/release
-    with open(os.path.join(releaseDir,fileName), 'r') as fileContent:
-      buildInfo = {} # Stores content of each release file
-      for buildName in fileContent:
-        buildInfo[getOSName(buildName)] = buildName.rstrip()
-      release['buildInfo'] = buildInfo
+    release['version'], release['date'] = os.path.splitext(file_name)[0].split("-",1)
+    # Read the build names of each release file.
+    with open(os.path.join(RELEASE_DIR,file_name), 'r') as content:
+      build_names = {}
+      for build_name in content:
+        build_names[get_os_name(build_name)] = build_name.rstrip()
+      release['build_names'] = build_names
     releases.append(release)
   return sorted(releases, key=lambda k : k['date'],reverse=True)
 
-def latestParaview():
-  paraviewFilenames = {} # Use a dict to store related OS name
-  # Obtain the latest version of paraview
-  with open(THIS_DIR + "/../releases/paraview/paraview.txt","r") as paraviewReleases:
-    version = paraviewReleases.readline().split(",")[1].rstrip()
-  # Obtain the build names for the latest version of paraview
-  with open(THIS_DIR + "/../releases/paraview/paraview-" + version + ".txt") as latestParaview:
-    for buildName in latestParaview:
-      paraviewFilenames[getOSName(buildName)] = buildName.rstrip()
-  return version, paraviewFilenames
+def latest_paraview_version():
+  """
+  Obtains the latest version of paraview in use by reading the first line of the paraview versions file.
 
-def getOSName(buildname):
+  Returns:
+    The version number of the latest paraview in use.
+  """
+  with open(os.path.join(PARAVIEW_DIR, "paraviewVersions.txt"), "r") as paraviewReleases:
+    return paraviewReleases.readline().split(",")[1].rstrip()
+
+def latest_paraview_release():
+  """
+  Reads the latest paraview release file, and writes the contents (which is the build names) to a list
+
+  Returns:
+    list: The build names for the latest version of paraview.
+  """
+  build_names = {} # Use a dict to store related OS name
+  # Obtain the build names for the latest version of paraview
+  filename = "paraview-" + latest_paraview_version() + ".txt"
+  with open(os.path.join(PARAVIEW_DIR, filename), "r") as latest_release:
+    for build_name in latest_release:
+      build_names[get_os_name(build_name)] = build_name.rstrip()
+  return build_names
+
+def get_os_name(build_name):
+  """
+  Obtains the operating system name from a given build name.
+
+  Args:
+    build_name (str): The name of the Mantid build for a given operating system, e.g. "mantid-2.3.2-SnowLeopard.dmg"
+
+  Returns:
+    str: The name of the operating system that the Mantid build will run on.
+  """
   osname = "unknown"
-  if "win64" in buildname or "Windows-64bit" in buildname: osname = "windows"
-  elif "win32" in buildname or "Windows-32bit" in buildname: osname = "win32"
-  elif "MountainLion" in buildname: osname = "mac"
-  elif "SnowLeopard" in buildname: osname = "snow-leopard"
-  elif ".rpm" in buildname: osname = "red-hat"
-  elif ".deb" in buildname or "Linux" in buildname: osname = "ubuntu"
-  elif "tar.gz" in buildname: osname = "source"
+  if "win64" in build_name or "Windows-64bit" in build_name: osname = "windows"
+  elif "win32" in build_name or "Windows-32bit" in build_name: osname = "win32"
+  elif "MountainLion" in build_name: osname = "mac"
+  elif "SnowLeopard" in build_name: osname = "snow-leopard"
+  elif ".rpm" in build_name: osname = "red-hat"
+  elif ".deb" in build_name or "Linux" in build_name: osname = "ubuntu"
+  elif "tar.gz" in build_name: osname = "source"
   return osname
 
-def getOSXCodeName(url):
+def get_osx_code_name(url):
+  """
+  Obtains the OSX code name from a given download url.
+  This is used as a custom filter in the jinja2 templating engine.
+
+  Args:
+    url (str): The download url of a mantid OSX build.
+
+  Returns:
+    str: The OSX code name, e.g. "Mountain Lion".
+  """
   url = url.replace(".dmg","")
   url = url.replace("-64bit","") # Required as some urls (paraview) have this
   url = url.rsplit("-",1)[1] # Obtain codename by splitting on last hyphen
@@ -56,27 +95,27 @@ def getOSXCodeName(url):
 
 if __name__ == "__main__":
   # Build information for each release file in the "releases" folder
-  releases = releases()
-  # Note: [0] is version, and [1] is buildnames.
-  latestParaview = latestParaview()
+  mantid_releases = mantid_releases()
+  # Note: [0] is version, and [1] is build_names.
+  latest_paraview = latest_paraview_release()
 
   # Variables to output on the archives page
   archiveVars = { "title" : "Mantid archive downloads",
                 "description" : "Downloads for current and previous releases of Mantid.",
-                "releases" : releases[1:] # Remove nightly (first item) build from releases
+                "releases" : mantid_releases[1:] # Remove nightly (first item) build from releases
                 }
 
   downloadVars = { "title" : "Mantid downloads",
                  "description" : "Download the latest release of Mantid.",
-                 "paraviewVersion" : latestParaview[0],
-                 "paraviewBuildNames" : latestParaview[1],
-                 "releases" : releases[0:2] # Only use first (nightly) and second (latest)
+                 "paraviewVersion" : latest_paraview_version(),
+                 "paraviewBuildNames" : latest_paraview,
+                 "releases" : mantid_releases[0:2] # Only use first (nightly) and second (latest)
                  }
 
   # Setup up the jinja environment and load the templates
-  env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=THIS_DIR + "/../templates"))
+  env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=os.path.join(ROOT_DIR,"templates")))
   # Add a filter to output the osx code name based on a given url
-  env.filters["getOSXCodeName"] = getOSXCodeName
+  env.filters["get_osx_code_name"] = get_osx_code_name
   # Write the contents of variables to the templates and dump the output to an HTML file.
-  env.get_template("archives.html").stream(archiveVars).dump(THIS_DIR +   "/../static/archives.html")
-  env.get_template("downloads.html").stream(downloadVars).dump(THIS_DIR + "/../static/index.html")
+  env.get_template("archives.html").stream(archiveVars).dump(os.path.join(ROOT_DIR + "static/archives.html"))
+  env.get_template("downloads.html").stream(downloadVars).dump(os.path.join(ROOT_DIR + "static/index.html"))
