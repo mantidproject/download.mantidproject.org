eae6884fa0b640c0673469a5dc507a40d074fc42
Jay Rainey <jawrainey@gmail.com>
2014-05-14 15:15:29 +0100

    Improve formatting, logic, and added docstrings in create-manifest.

--- tools/create-manifest.py
+++ tools/create-manifest.py
@@ -6,8 +6,50 @@ import datetime
 import os
 import sys
 
+def update_paraview_versions(mantid_version, paraview_version):
+  """
+  Adds the mantid release code and paraview version to the top of the paraviewReleases file.
+
+  Args:
+    mantid_version (str): The version number of the given Mantid release.
+    paraview_version (str): The paraview version that this release of Mantid will use.
+
+  """
+  with open(os.path.join(RELEASE_DIR, "paraview/paraviewVersions.txt"), "r+") as paraview_versions:
+    # Read entire file to enable insertion to start of file.
+    content = paraview_versions.read()
+    # If no paraview version is provided, then the previous paraview version is used.
+    previous_version = content.split("\n",1)[0].split(",")[1]
+    if not paraview_version: paraview_version = previous_version
+    # Add the new release and paraview version to the top of the file
+    paraview_versions.seek(0,0)
+    paraview_versions.write(mantid_version + "," + paraview_version + "\n" + content)
+    print "The following was added to the paraviewVersions file: " + mantid_version + " " + paraview_version
+
+def create_release_file(version, date):
+  """
+  Creates a file in the release folder and writes the build names to it for a given release.
+
+  Args:
+    version (str): The version code for this release.
+    date (str): The date that this version of Mantid was released.
+
+  """
+  with open(os.path.join(RELEASE_DIR, version + "-" + date + ".txt"), "w+") as release_file:
+    release_file.write('\n'.join([build_name%(args.version) for build_name in MANTID_BUILD_NAMES]))
+    print "The new release file was created successfully."
+
 if __name__ == "__main__":
-  RELEASE_DIR = os.path.abspath(os.path.dirname(__file__)) + "/../releases/"
+
+  # TODO: Update if a build name changes with a release.
+  MANTID_BUILD_NAMES = [
+   "mantid-%s-win64.exe",
+   "mantid-%s-MountainLion.dmg",
+   "mantid-%s-1.el6.x86_64.rpm",
+   "mantid_%s-1_amd64.deb",
+   "mantid-%s.tar.gz"]
+
+  RELEASE_DIR = os.path.join(os.path.dirname(__file__),"../releases")
 
   parser = argparse.ArgumentParser(prog='create-manifest', usage='%(prog)s [options]',
       description='Creates a release file in the "releases" folder with the name provided.')
@@ -29,26 +71,5 @@ if __name__ == "__main__":
     except ValueError:
       sys.exit("The date you have provided is invalid. It must be in Y-M-D format.")
 
-  # Use the previous version of paraview if none is provided.
-  if not args.paraview:
-    with open(RELEASE_DIR + "paraview.txt", "r") as paraviewReleases:
-      args.paraview = paraviewReleases.readline().split(",")[1].rstrip()
-
-  # TODO: Update if a build name changes with a release.
-  buildnames = [
-   "mantid-RNUM-win64.exe",
-   "mantid-RNUM-MountainLion.dmg",
-   "mantid-RNUM-1.el6.x86_64.rpm",
-   "mantid_RNUM-1_amd64.deb",
-   "mantid-RNUM.tar.gz"]
-
-  with open(RELEASE_DIR + args.version + "-" + str(args.date) + ".txt", "w") as releaseFile:
-    releaseFile.write('\n'.join([name.replace("RNUM",args.version) for name in buildnames]))
-
-  # Write the paraview release to the start of the paraview file
-  with open(RELEASE_DIR + "paraview.txt", "r+") as paraviewReleases:
-    content = paraviewReleases.read()
-    paraviewReleases.seek(0,0)
-    paraviewReleases.write(args.version + "," + args.paraview + "\n" + content)
-
-  sys.exit("Success: Created release file (" + args.version + ")" + " in " + RELEASE_DIR)
+  update_paraview_versions(args.version,args.paraview)
+  create_release_file(args.version,str(args.date))
