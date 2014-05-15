9fbe4b91b727af36445983570a03dae21165f8c4
Jay Rainey <jawrainey@gmail.com>
2014-05-15 14:31:29 +0100

    Added create paraview file functionality.
    - Manually added the previous paraview release file (it has different build names than current).

--- tools/create-manifest.py
+++ tools/create-manifest.py
@@ -3,19 +3,21 @@
 
 import argparse
 import datetime
+import distutils.version
 import os
 import sys
+
 
 def update_paraview_versions(mantid_version, paraview_version):
   """
   Adds the mantid release code and paraview version to the top of the paraviewReleases file.
+  If paraview_version is greater than previous paraview version a new paraview build file is created.
 
   Args:
     mantid_version (str): The version number of the given Mantid release.
     paraview_version (str): The paraview version that this release of Mantid will use.
-
   """
-  with open(os.path.join(RELEASE_DIR, "paraview/paraviewVersions.txt"), "r+") as paraview_versions:
+  with open(os.path.join(PARAVIEW_DIR, "paraviewVersions.txt"), "r+") as paraview_versions:
     # Read entire file to enable insertion to start of file.
     content = paraview_versions.read()
     # If no paraview version is provided, then the previous paraview version is used.
@@ -25,6 +27,9 @@ def update_paraview_versions(mantid_version, paraview_version):
     paraview_versions.seek(0,0)
     paraview_versions.write(mantid_version + "," + paraview_version + "\n" + content)
     print "The following was added to the paraviewVersions file: " + mantid_version + " " + paraview_version
+    # Compare paraview versions, and create a new ('latest') paraview file if provided version is greater than previous
+    if distutils.version.StrictVersion(paraview_version) > distutils.version.StrictVersion(previous_version):
+      create_paraview_file(paraview_version)
 
 def create_release_file(version, date):
   """
@@ -33,11 +38,20 @@ def create_release_file(version, date):
   Args:
     version (str): The version code for this release.
     date (str): The date that this version of Mantid was released.
-
   """
   with open(os.path.join(RELEASE_DIR, version + "-" + date + ".txt"), "w+") as release_file:
-    release_file.write('\n'.join([build_name%(args.version) for build_name in MANTID_BUILD_NAMES]))
+    release_file.write('\n'.join([build_name%(version) for build_name in MANTID_BUILD_NAMES]))
     print "The new release file was created successfully."
+
+def create_paraview_file(version):
+  """
+  Creates a new paraview build file in the paraview folder with a given version name.
+
+  Args:
+    version (str): Used to name the file, and name the builds within the file.
+  """
+  with open(os.path.join(PARAVIEW_DIR, "paraview-" + version + ".txt"), "w+") as release_file:
+    release_file.write('\n'.join([build_name%(version) for build_name in PARAVIEW_BUILD_NAMES]))
 
 if __name__ == "__main__":
 
@@ -49,7 +63,17 @@ if __name__ == "__main__":
    "mantid_%s-1_amd64.deb",
    "mantid-%s.tar.gz"]
 
+  # TODO: Update if a paraview build name changes with a new supported release.
+  PARAVIEW_BUILD_NAMES = [
+   "ParaView-%s-Windows-64bit.exe",
+   "ParaView-%s-Windows-32bit.exe",
+   "ParaView-%s-SnowLeopard-64bit.dmg",
+   "ParaView-%s-MountainLion-64bit.dmg",
+   "ParaView-%s-Linux-64bit.tar.gz",
+   "ParaView-%s-source.tar.gz"]
+
   RELEASE_DIR = os.path.join(os.path.dirname(__file__),"../releases")
+  PARAVIEW_DIR = os.path.join(RELEASE_DIR, "paraview/")
 
   parser = argparse.ArgumentParser(prog='create-manifest', usage='%(prog)s [options]',
       description='Creates a release file in the "releases" folder with the name provided.')
@@ -59,7 +83,8 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   # Validate version
-  if len(args.version) > 5: sys.exit("Error: Invalid version number provided.")
+  if len(args.version) > 5:
+    sys.exit("Error: Invalid version number provided.")
 
   # Use current date if none provided
   if not args.date:
