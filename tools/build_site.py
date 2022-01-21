851851edbf9d608447016cfc253feb55d139ddac
David Fairbrother <DavidFair@users.noreply.github.com>
2022-01-21 12:09:20 +0000

    Split up and overhaul generation scripts
    - Chunk major steps into separate componenets
    - Introduce data classes instead of lists
    - Rejig loops / os.path usage to pathlib
    - Add tests with pytest
    - Update Jinja to understand new data structs
    - Drop Paraview support / mentions (these were only lists for 3.3)
    - Add venv instructions

---
+++ tools/build_site.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+
+import docutils.core
+import jinja2
+
+# General globals
+from impl import url_handling
+from impl.release_parsing import get_mantid_releases, get_nightly_release
+from impl.static_vars import INSTRUCTIONS_DIR, ROOT_DIR
+
+MANTID_NEWS = "https://developer.mantidproject.org/"
+
+if __name__ == "__main__":
+    # Build information for each release file in the "releases" folder
+    release_info = get_mantid_releases()
+
+    # Variables to output on the archives page
+    archive_vars = {
+        "title": "Mantid - Previous Releases",
+        "description": "Downloads for current and previous releases of Mantid.",
+        "releases": release_info[1:]
+    }
+
+    latest_version = release_info[0]
+    download_vars = {
+        "title": "Mantid - Downloads",
+        "description": "Download the latest release of Mantid.",
+        "sample_datasets": url_handling.SAMPLES_DATASETS,
+        "mantid_news": MANTID_NEWS,
+        "release_notes": latest_version.release_notes_url,
+        "latest_release": latest_version,
+        "nightly_release": get_nightly_release(),
+        "instructions": [
+            os.path.splitext(filename)[0] for filename in sorted(os.listdir(INSTRUCTIONS_DIR))
+        ],
+        "ipython_notebook": url_handling.IPYTHON_NOTEBOOK
+    }
+
+    # Setup up the jinja environment and load the templates
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(
+        searchpath=os.path.join(ROOT_DIR, "templates")))
+    # Write the contents of variables to the templates and dump the output to an HTML file.
+    env.get_template("archives.html").stream(archive_vars).dump(
+        os.path.join(ROOT_DIR, "docs", "archives.html"))
+    env.get_template("downloads.html").stream(download_vars).dump(
+        os.path.join(ROOT_DIR, "docs", "index.html"))
+
+    for instruction_file in sorted(os.listdir(INSTRUCTIONS_DIR)):
+        with open(os.path.join(INSTRUCTIONS_DIR, instruction_file), "r") as content:
+            # Converts ReST file contents to HTML.
+            parts = docutils.core.publish_parts(content.read(),
+                                                writer_name='html',
+                                                settings_overrides={'doctitle_xform': False})
+            # Required to disable promotion of top level header to section title.
+
+            # Remove extension from instruction filename
+            filename = os.path.splitext(instruction_file)[0]
+            filename = filename.replace("-", " ")
+
+            instruction_vars = {
+                "title": filename + " installation instructions for Mantid.",
+                "description": "Mantid installation instructions for " + filename + ".",
+                "instructions": parts["html_body"]
+            }
+
+            env.get_template("instructions.html").stream(instruction_vars).dump(
+                os.path.join(ROOT_DIR, "docs",
+                             filename.replace(" ", "").lower() + ".html"))
---
+++ tools/impl/release_parsing.py
@@ -0,0 +1,114 @@
+import re
+import datetime
+from pathlib import Path
+from typing import Optional, List
+
+from dateutil import parser
+
+from impl.os_handling import get_os
+from impl.package_details import PackageDetails
+from impl.release_info import ReleaseInfo
+from impl.static_vars import RELEASE_DIR, NIGHTLY_NAME_SUFFIX
+from impl.url_handling import get_download_url
+
+
+def get_nightly_date(package_name: str) -> Optional[datetime.date]:
+    split_names = package_name.split('.')
+    for name in split_names:
+        try:
+            date = parser.isoparse(name)
+            return date.date()  # drop time data
+        except ValueError:
+            pass
+
+
+def format_release_str(release_str: str) -> str:
+    """
+    Takes a release string, and formats it using the convention we use elsewhere
+    in Mantid, for example DOI's.  Essentially, the patch number is removed if
+    it is zero.  I.e. 2.1.0 becomes 2.1, 3.0.0 becomes 3.0, but 3.0 is left alone.
+
+    Args:
+      release_str (str) :: the release string to format.
+
+    Returns:
+      str: the formatted release string
+    """
+    if re.match(r"\d\.\d\.0", release_str):
+        return release_str[:-2]
+    return release_str
+
+
+def _parse_build_names(file_path: Path) -> ReleaseInfo:
+    """
+    Parses a file that contains build names (e.g. those in /releases/) and stores the contents in a dictionary.
+    The key of the dictionary is the operating system, which is obtained based on the build's file extension.
+    The date on the first line is optional, if it is not present the date is extracted from the first filename if
+    possible, else it raises an error.
+
+    Args:
+      file_path (str): The location of the release file to parse.
+
+    Returns:
+      ReleaseInfo object containing details about a release
+    """
+    file_name = file_path.name
+    is_nightly = NIGHTLY_NAME_SUFFIX.casefold() in file_name.casefold()
+
+    if is_nightly:
+        version = "nightly"
+    else:
+        version = file_path.with_suffix("").name
+
+    formatted_version = None if is_nightly else format_release_str(version)
+
+    with open(file_path) as handle:
+        # Strip and get non-empty lines
+        parsed_file_names = [line.strip() for line in handle if line.strip()]
+
+    # All non-nightly files have the date as their first line
+    if not is_nightly:
+        release_date = datetime.date.fromisoformat(parsed_file_names.pop(0))
+    else:
+        release_date = get_nightly_date(parsed_file_names[0])
+
+    package_details: List[PackageDetails] = []
+
+    for executable_name in parsed_file_names:
+        package_details.append(
+            PackageDetails(os_details=get_os(executable_name, version),
+                           download_url=get_download_url(executable_name, version, is_nightly))
+        )
+
+    return ReleaseInfo(date=release_date, version=version,
+                       formatted_version=formatted_version, package_details=package_details)
+
+
+def get_nightly_release() -> ReleaseInfo:
+    """
+    Reads and stores release information for the nightly build from the nightly text file in the releases folder.
+    The date on the first line is optional, if it is not present the date is extracted from the first filename.
+
+    Return:
+      dict: A dictionary containing release information for the nightly build.
+      A similar format (see inner dict) of get_mantid_releases above is returned.
+    """
+    filename = [name for name in RELEASE_DIR.iterdir() if NIGHTLY_NAME_SUFFIX in str(name)]
+    assert len(filename) == 1
+    return _parse_build_names(filename[0])
+
+
+def get_mantid_releases() -> List[ReleaseInfo]:
+    """
+    Reads and stores release information for each release file in the releases folder.
+    This does not include the nightly build.
+
+    Returns:
+      list: A list that contains release information for each release. The list is sorted by release date.
+    """
+    releases: List[ReleaseInfo] = []
+    release_files = [path for path in RELEASE_DIR.iterdir() if NIGHTLY_NAME_SUFFIX not in str(path)]
+    for release_txt_file in release_files:
+        releases.append(_parse_build_names(release_txt_file))
+
+    return sorted(releases, key=lambda k: k.date, reverse=True)
